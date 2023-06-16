1f6e033824de134a779d369a901fc35b2198a393
Reva Doni Aprilio <aprilio842@gmail.com>
2023-06-16 19:26:10 +0700

    Update AritmatikaTest.java

--- src/test/java/AritmatikaTest.java
+++ src/test/java/AritmatikaTest.java
@@ -6,25 +6,25 @@ import org.junit.Test;
 
 public class AritmatikaTest {
 
-  @test
+  @Test
   public void testTambah() {
     Aritmatika aritmatika = new Aritmatika(15, 3);
     assertEquals(aritmatika.tambah(), 18);
   }
 
-  @test
+  @Test
   public void testKurang() {
     Aritmatika aritmatika = new Aritmatika(20, 10);
     assertEquals(aritmatika.kurang(), 10);
   }
 
-  @test
+  @Test
   public void testKali() {
     Aritmatika aritmatika = new Aritmatika(20, 5);
     assertEquals(aritmatika.kali(), 100);
   }
 
-  @test
+  @Test
   public void testBagi() {
     Aritmatika aritmatika = new Aritmatika(120, 4);
     assertEquals(aritmatika.bagi(), 30);
