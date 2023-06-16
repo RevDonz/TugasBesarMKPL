2c635a624b52cf15d30f5a5df5dabfdf4fac4baf
Reva Doni Aprilio <aprilio842@gmail.com>
2023-06-16 21:06:13 +0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -15,5 +15,12 @@ public class Driver {
         
         int hasilBagi = aritmatika.bagi();
         System.out.println("Hasil Pembagian: " + hasilBagi);
+        System.out.println("Hasil Pembagian: " + hasilBagi);
+        System.out.println("Hasil Pembagian: " + hasilBagi);
+        System.out.println("Hasil Pembagian: " + hasilBagi);
+        System.out.println("Hasil Pembagian: " + hasilBagi);
+        System.out.println("Hasil Pembagian: " + hasilBagi);
+        System.out.println("Hasil Pembagian: " + hasilBagi);
+        System.out.println("Hasil Pembagian: " + hasilBagi);
     }
 }
